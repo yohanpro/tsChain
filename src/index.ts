5ba91a40eb022c354e26fe2ac4cc2aa4f83d3406
Yohan <yohan@yohanpro.com>
2020-08-02 20:27:40 +0900

    #9 BlockChain Crpytojs 추가

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
@@ -5,6 +7,14 @@ class Block {
   public data: string;
   public timestamp: number;
 
+  static calculateHash = (
+    index: number,
+    previousHash: string,
+    timestamp: number,
+    data: string
+  ): string => {
+    return CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
+  };
   constructor(
     index: number,
     hash: string,
@@ -19,6 +29,38 @@ class Block {
     this.timestamp = timestamp;
   }
 }
+
 const genesisBlock: Block = new Block(0, "abcd02", "", "hi", 123456);
 
-let blockChain: [Block] = [genesisBlock];
+let blockChain: Block[] = [genesisBlock];
+
+const getBlockChain = (): Block[] => blockChain;
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+
+  const newHash: string = Block.calculateHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+  return newBlock;
+};
+
+console.log(createNewBlock("hello"));
+console.log(createNewBlock("bye bye"));
+
+export {};
